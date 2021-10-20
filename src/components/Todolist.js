60eff767cff0b723a03c58d31bb4f6587e67a0d2
ntran402 <56668766+ntran402@users.noreply.github.com>
2021-10-19 22:38:42 -0700

    Add files via upload

---
+++ src/components/Todolist.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Todo from "./Todo";
+
+const Todolist = ({ todos, setTodos, filteredTodos }) => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            todos={todos}
+            setTodos={setTodos}
+            key={todo.id}
+            todo={todo}
+            text={todo.text}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todolist;
